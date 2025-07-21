8538f15f27ccfae548d6cab6202a831840048569
Yahia Mabrouk <yahiamabrouk1981@gmail.com>
2025-07-21 09:11:41 +0100

    some changes

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.use("/api/chantiers", chantierRoutes);
 app.use("/api/charges", chargeRoutes);
 app.use("/api/salaries", salarieRoutes);
 app.use("/api/fournisseurs", fournisseurRoutes);
-
+app.use("/", (req, res) => {
+  res.send("Welcome to the API");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
