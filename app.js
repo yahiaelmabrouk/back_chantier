678a2a8a986071ba8dcf679d7f52d7924b4670ec
Yahia Mabrouk <yahiamabrouk1981@gmail.com>
2025-09-22 11:12:26 +0100

    fixing cors

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const cors = require("cors");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://gestioncash.com'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
